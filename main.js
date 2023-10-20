a044d66bf5a2012db2659211648d014c52b502bb
venske <105080911+venske@users.noreply.github.com>
2023-10-20 10:46:18 -0300

    aa

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
+//Declaraão/criação da função tocaSomPom
 function tocaSomPom(){  
     document.querySelector("#som_trcla_pom").play();
+}
+//chamada ou invocação da função tocaSomPom ao clicar no botão
+document.querySelector(' . tecla_pom').onclick = tocaSomPom;
