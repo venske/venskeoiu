e932a14f210423f4328ab5ebe617fcae76af84a0
venske <105080911+venske@users.noreply.github.com>
2023-10-20 11:06:05 -0300

    ll

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function tocaSomPom(){
 }
 //chamada ou invocação da função tocaSomPom ao clicar no botão
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+
+//Declaraão/criação da função tocaSomClap
+function tocaSomClap(){  
+    document.querySelector("#som_tecla_clap").play();
+}
+//chamada ou invocação da função tocaSomClap ao clicar no botão
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
