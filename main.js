7322df0c6adf134037cd498e569adfab30066143
venske <105080911+venske@users.noreply.github.com>
2023-10-27 10:23:51 -0300

    kjk

--- main.js
+++ main.js
@@ -2,19 +2,12 @@
 function tocaSomPom(){  
     document.querySelector("#som_tecla_pom").play();
 }
-//chamada ou invocação da função tocaSomPom ao clicar no botão
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
-
-//Declaraão/criação da função tocaSomClap
-function tocaSomClap(){  
-    document.querySelector("#som_tecla_clap").play();
-}
-//chamada ou invocação da função tocaSomClap ao clicar no botão
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 //criando a refêrencias constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 //chamando a constante listaDeTeclas e o índice "0" do somPom
-listaDeTeclas[0].onclick = tocaSomPom;
+
+let comtadoe = 0;
+while(contador < 9){  
+    listaDeTeclas[0].onclick = tocaSomPom;
+} 
