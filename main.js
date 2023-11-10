ba4ebed792fd8f6fab1be5ae3b9d6b7eb096c79a
venske <105080911+venske@users.noreply.github.com>
2023-11-10 11:10:28 -0300

    aa

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 //ceiação da repetição "while" e trocado pelo "for" com variavel,condição e incremento
 for(let contador = 0;contador < listaDeTeclas.lengtht; contador++){  
     const teclas = listaDeTeclas[contador]; //guarda o contador da lista
-    const efeito = teclas.classList[1]; //chama o 2 item classe individual do som
+    const efeito = teclas.classlist[1]; //chama o 2 item classe individual do som
     const idAudio = `#som_"${efeito}`; //utiliza a junção template string ${ }
     teclas.onclick = function(){      //função anônima auxiliar para chamar tocaSom
         tocaSomPom(idAudio);      //chamada da função tocaSom com parâmetro idAudio
