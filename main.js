b03a42bbbfbab8c100317d11f92c8ba601775a13
venske <105080911+venske@users.noreply.github.com>
2023-11-10 11:08:52 -0300

    jj

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ function tocaSomPom(ideElementoAudio){
 }
 //criando a refêrencias constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-for(let contador = 0;contador < listaDeTeclas.lengthlet; contador++){  
-    const teclas = listaDeTeclas[contador]
-    const efeito = teclas.classList[1];
-    const idAudio = `#som_"${efeito}`;
-    teclas.onclick = function(){   
-        tocaSomPom(idAudio);
+//ceiação da repetição "while" e trocado pelo "for" com variavel,condição e increto
+for(let contador = 0;contador < listaDeTeclas.lengtht; contador++){  
+    const teclas = listaDeTeclas[contador]; //guarda o contador da lista
+    const efeito = teclas.classList[1]; //chama o 2 item classe individual do som
+    const idAudio = `#som_"${efeito}`; //utiliza a junção template string ${ }
+    teclas.onclick = function(){      //função anônima auxiliar para chamar tocaSom
+        tocaSomPom(idAudio);      //chamada da função tocaSom com parâmetro idAudio
     }
 } 
