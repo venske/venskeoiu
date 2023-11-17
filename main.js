f74d3dd5683ad48060e1bca36e8575481c07c2ce
venske <105080911+venske@users.noreply.github.com>
2023-11-17 10:51:59 -0300

    kk

--- main.js
+++ main.js
@@ -12,4 +12,9 @@ for(let contador = 0;contador < listaDeTeclas.length; contador++){
     teclas.onclick = function(){      //função anônima auxiliar para chamar tocaSom
         tocaSom(idAudio);      //chamada da função tocaSom com parâmetro idAudio
     }
+    teclas.onkeydown = function(){  
+        teclas.classList.add('ativa');
+} 
+teclas.onkeydown = function(){  
+    teclas.classList.add('ativa')
 } 
