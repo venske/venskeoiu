fee4874666fde324520d0b5153405dc6abe9f8e1
venske <105080911+venske@users.noreply.github.com>
2023-11-24 11:04:00 -0300

    kk

--- main.js
+++ main.js
@@ -9,14 +9,20 @@ for(let contador = 0;contador < listaDeTeclas.length; contador++){
     const teclas = listaDeTeclas[contador]; //guarda o contador da lista
     const efeito = teclas.classList[1]; //chama o 2 item classe individual do som
     const idAudio = `#som_${efeito}`; //utiliza a junção template string ${ }
+   
     teclas.onclick = function(){      //função anônima auxiliar para chamar tocaSom
         tocaSom(idAudio);      //chamada da função tocaSom com parâmetro idAudio
     }
     teclas.onkeydown = function(evento){  
-        if(evento.code != 'Tab'){  
-        teclas.classList.add('ativa');
+       
+        console.log(evento)
+        
+        if ('Espaço') {   
+           if(evento.code != 'Tab'){  
+           teclas.classList.add('ativa');
         }
-    } 
+    }
+    
     teclas.onkeyup = function(){  
         teclas.classList.remove('ativa');
     } 
